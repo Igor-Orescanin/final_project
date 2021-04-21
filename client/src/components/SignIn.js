06e2deaa8cad158adb51f202f7791ffd7bb6ff9b
Marika <MarikaHasse@googlemail.com>
2021-04-21 11:43:20 +0200

    sign-in

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -3,22 +3,15 @@ import React from 'react'
 //styles
 import { makeStyles } from '@material-ui/core/styles'; //generel
 import{Container} from '@material-ui/core'
+import Typography from '@material-ui/core/Typography'; //Typography
 import AccountCircleIcon from '@material-ui/icons/AccountCircle'; //icon
 import Button from '@material-ui/core/Button'; //button
 import Avatar from '@material-ui/core/Avatar'; //avatar
-//import useStyles from './styles';
+import useStyles from './styles';
 
-//import StorageIcon from '@material-ui/icons/Storage';
+
 
 import { createMuiTheme,ThemeProvider } from '@material-ui/core';
-
-import useStyles from "./styles"
-
-//const classes = useStyles();
-
-
-
-
 
 
 
@@ -28,15 +21,16 @@ const SignIn = () => {
 
     return (
         <Container component="main" maxWidth="xs" >
-        <div> 
+       <Typography className={classes.typography} component="h1" variant="h5">
+          Sign-In
+        </Typography>
        
-             <Avatar variant='circle' className={classes.myColor} > 
+             <Avatar variant='rounded' className={classes.myColor} > 
                  {/* <AccountCircleIcon className={classes.otherColor} fontSize="large"/>  */}
                 
              </Avatar> 
           
-        </div>
-        
+     
           
         </Container>
     )
--- client/src/components/styles.js
+++ client/src/components/styles.js
@@ -1,3 +1,4 @@
+import { Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 
@@ -7,12 +8,24 @@ export default makeStyles((theme)=>({
   myColor: {
       backgroundColor:"#007982",
      
-      width:'100px'
+      width:'80px',
+      height:'80px'
    
     },
     otherColor: {
       backgroundColor: "#007982"
       
+    },
+    typography:{
+      variant:"body2",
+      color:"textSecondary",
+      align:"center",
+      color: "#007982",
+      fontSize:'32px',
+      fontWeight:'bold'
     }
-
 }))
+
+// 25.47.119.246
+// igor 25.45.213.82
+// maritza 25.47.109.138
