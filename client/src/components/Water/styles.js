3ad2e029e755298a3417cdf95e4f405b046acce9
Marika <MarikaHasse@googlemail.com>
2021-04-28 21:20:01 +0200

    charts in water and styles

--- client/src/components/Water/styles.js
+++ client/src/components/Water/styles.js
@@ -10,4 +10,21 @@ navContainer:{
     borderBottom:'4px solid #0C9EB5',
     
   },
+//_______________maritza
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+
+// ________ color: theme.palette.text.secondary,test
+
+
+chart:{
+  color: theme.palette.text.secondary,
+}
+
 }))
