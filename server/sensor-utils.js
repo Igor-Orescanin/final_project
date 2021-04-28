c2de11780fc692a9a67b60af24d6ea9cd560a083
Maritza <maritzaochrom@gmail.com>
2021-04-28 09:27:06 +0200

    server and fakeSensor

---
+++ server/sensor-utils.js
@@ -0,0 +1,40 @@
+const Raspi = require('raspi');
+const I2C = require('raspi-i2c').I2C;
+const ADS1x15 = require('raspi-kit-ads1x15');
+
+const { Sensor } = require('./Sensor');
+const { WaterType } = require('./WaterType');
+
+function initRaspberry() {
+    return new Promise((resolve) => {
+        Raspi.init(() => resolve(undefined));
+    });
+}
+
+async function getSensor(readingInterval = 500) {
+    await initRaspberry();
+
+    // Init Raspi-I2c
+    const i2c = new I2C();
+    const adcSensor = new ADS1x15({
+        i2c,                                    // i2c interface
+        chip: ADS1x15.chips.IC_ADS1015,         // chip model
+        address: ADS1x15.address.ADDRESS_0x48,  // i2c address on the bus
+
+        // Defaults for future readings
+        pga: ADS1x15.pga.PGA_4_096V,            // power-gain-amplifier range
+        sps: ADS1x15.spsADS1015.SPS_250,        // data rate (samples per second),
+        spsExtraDelay: 1,                       // Additional number of milliseconds to delay in addition to the reading time (based on sps)
+    });
+    const channelsMap = new Map();
+
+    channelsMap.set(WaterType.CLEAN, ADS1x15.channel.CHANNEL_0);
+    channelsMap.set(WaterType.WASTE, ADS1x15.channel.CHANNEL_1);
+
+    return new Sensor(adcSensor, channelsMap, readingInterval);
+}
+
+module.exports = {
+    getSensor,
+};
+
