f118335a93c2b9d6e854d7b5f6c88f42f9be1a78
Sandeep <deinsangwan@gmail.com>
2021-04-30 11:37:15 +0200

    edited axios

--- client/src/components/axios.js
+++ client/src/components/axios.js
@@ -1,7 +1,25 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:4000/app/registration'
+    baseURL: 'http://maritza.localhost:4000/app/registration'
 })
 
 export default instance;
+
+
+
+
+/* 
+import Axios from "axios";
+
+
+Axios({
+    method: "GET",
+    url: "http://localhost:5000/",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(res => {
+    console.log(res.data.message);
+  });
+   */
